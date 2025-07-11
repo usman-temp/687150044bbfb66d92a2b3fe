25bff0234df45613a16f8305d2cdc1d76c961c86
usman-temp <usmanjaved1620@gmail.com>
2025-07-11 22:56:23 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
